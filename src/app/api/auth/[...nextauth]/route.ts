ca564421dc90b8bed0c55891a7d2032034371a69
harshit kudhial <122823756+harshitnub077@users.noreply.github.com>
2026-04-11 19:32:22 +0530

    feat: implement authentication layer with NextAuth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  // pages: {
+  //   signIn: "/auth/signin", 
+  // },
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        // @ts-ignore
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
